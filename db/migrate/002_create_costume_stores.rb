dac54e202e2f1d5020668456aceb9909d8ef2240
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-29 01:07:44 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+
+class CreateCostumesStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
